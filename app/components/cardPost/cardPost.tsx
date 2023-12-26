6d751b5d5e506a955c189963b7c48018532029f6
Dju7 <jlien7@gmail.com>
2023-12-26 15:17:45 +0100

    text-content-ellipsis

--- app/components/cardPost/cardPost.tsx
+++ app/components/cardPost/cardPost.tsx
@@ -12,7 +12,7 @@ const CardPost:React.FC<PostProps>= ({ id, title, content, author }) => {
         <div className="bg-secondary w-full h-[30%] text-clearBlue shadow-xl shadow-primary p-2">
             <h2 className="text-2xl text-clearBlue font-bold text-center border-b border-clearBlue mb-4">{title}</h2>
             <div className="flex flex-col justify-between h-[80%]">
-                <p className="mb-4">{content || "contenu non disponible"}</p>   
+                <p className="mb-4 text-ellipsis">{content || "contenu non disponible"}</p>   
                 <p className="text-sm text-right">écrit par: {author}</p>
             </div>
         </div>
